61472db918592fcb14991997f00391c2f54d3dd2
Anubhav Singh <Anubhav@Anubhavs-MacBook-Air.local>
2025-12-30 20:48:30 +0530

    some changes

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("")
+
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+})
+
+const todos = mongoose.model('todos', todoSchema);
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const { createTodos, updateTodos } = require("./types");
 const app = express();
 
 app.use(express.json());
 
 app.post("/todos", function(req, res){
 
+    const createPayload = req.body;
+    const parsedPayload = createTodos.safeParse(createPayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "You provided the wrong inputs",
+        })
+        return;
+    }
+    //put in mongo or whatever afterwards
 })
 
 app.get("/todos", function(req, res){
@@ -13,4 +23,12 @@ app.get("/todos", function(req, res){
 
 app.put("/completed", function(req, res){
     
+    const updatePayload = req.body;
+    const parsedPayload = updateTodos.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "You provided the wrong inputs",
+        })
+        return;
+    }
 })
